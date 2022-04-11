a49fea916fc3f19c713bed7b136a61c92322e3b2
Alex Asriyan <aasriyan01@gmail.com>
2022-04-11 11:33:25 -0400

    added ability to add movies show in a list

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,19 +1,53 @@
-const movieList = document.getElementById("movie-list");
+const d = document;
+const addMovieBtn = d.getElementById("add-movie-btn");
+const searchBtn = d.getElementById("search-btn");
 
-movieList.style.backgroundColor = "red";
-movieList.style.display = "block";
+const movies = [];
 
-let person = {
-	name: "Max",
-	age: 30,
-	hobbies: ["Sports", "Cooking"],
-	greet: function() {
-		alert("Hi there!");
-	},
-	1.5: "hello"
+const renderMovies = () => {
+	const movieList = d.getElementById("movie-list");
+
+	if (movies.length === 0) {
+		movieList.classList.remove('visible');
+	} else {
+		movieList.classList.add("visible");
+	}
+	movieList.innerHTML = "";
+
+	movies.forEach(movie => {
+		const movieEl = d.createElement("li");
+		let text = movie.info.title + " - ";
+
+		for (const key in movie.info) {
+			if (key !== "title") {
+				text = text + `${key}: ${movie.info[key]}`
+			}
+		}
+		
+		movieEl.textContent = text;
+		movieList.append(movieEl);
+	})
 };
 
-delete person.age;
-person.isAdmin = true;
-console.log(person);
-console.log(person[1.5])
+const addMovieHandler = () => {
+	const title = d.getElementById("title").value;
+	const extraName = d.getElementById("extra-name").value;
+	const extraValue = d.getElementById("extra-value").value;
+
+	if (title.trim() === "" || extraName.trim() === "" || extraValue.trim() === "") {
+		return;
+	}
+
+	const newMovie = {
+		info: {
+			title,
+			[extraName]: extraValue
+		},
+		id: Math.random()
+	};
+
+	movies.push(newMovie);
+	renderMovies();
+};
+
+addMovieBtn.addEventListener("click", addMovieHandler);
